4ef0bc0061757dd65b7be193825e259637907d51
dogdog7788 <dogdog7788@qq.com>
2016-08-22 17:42:46 +0800

    key修改

--- submsg-pay/src/main/java/cn/submsg/pay/alipay/directpay/config/AlipayConfig.java
+++ submsg-pay/src/main/java/cn/submsg/pay/alipay/directpay/config/AlipayConfig.java
@@ -16,7 +16,7 @@ public class AlipayConfig {
 //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 	// 合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
-	public static String partner = "2088102577012481";
+	public static String partner = "2088421673493740";
 	// 收款支付宝账号，以2088开头由16位纯数字组成的字符串，一般情况下收款账号就是签约账号
 	public static String seller_id = partner;
 	//商户的私钥,需要PKCS8格式，RSA公私钥生成：https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
